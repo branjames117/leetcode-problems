6e02285d90ea6e3631bb8a287884c0adbe553ba6
Brandon Hoskins <branjames117@gmail.com>
2022-04-06 22:16:55 -0500

    commented out mergeTwoSortedLists.js

--- problems/mergeTwoSortedLists.js
+++ problems/mergeTwoSortedLists.js
@@ -49,10 +49,31 @@ var mergeTwoLists = function (list1, list2) {
 };
 
 // solution 2, from Sonika Maheshwari at https://sonikamaheshwari067.medium.com/javascript-merge-two-sorted-lisnked-list-1233b0bdfb6f
+
+// lay the groundwork
+class Node {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
 var mergeTwoLists = function (list1, list2) {
+  // create a new linked list with its first node
   let finalList = new Node();
+
+  /* 
+  At this point, finalList looks like this:
+  {
+    val: 0,
+    next: null
+  }
+  */
+
+  // establish it as the head of the list so we don't lose the reference to the head as we build it out
   let head = finalList;
 
+  // as long as a NEXT exists in both input lists, carry on
   while (list1 !== null && list2 !== null) {
     // Select the smallest value from either linked list,
     // then increment that list forward.
@@ -65,6 +86,32 @@ var mergeTwoLists = function (list1, list2) {
       list2 = list2.next;
     }
 
+    /*
+    After the first iteration, finalList will look like this:
+    {
+      val: 0,
+      next: {
+        val: 1,
+        next: null
+      }
+    }
+
+    After the second iteration, finalList will look like this:
+    {
+      val: 0,
+      next: {
+        val: 1,
+        next: {
+          val: 2,
+          next: null,
+        }
+      }
+    }
+
+    And so on, so in each comparison check we are seeing which of the currently tracked value in either list is the lowest, then setting that as the value of the next object in our new list, while also moving the pointer forward in the list that won out
+    */
+
+    // move our reference to the list down a level
     finalList = finalList.next;
   }
 
@@ -76,3 +123,5 @@ var mergeTwoLists = function (list1, list2) {
   // return .next because this first element in the linkedlist is empty
   return head.next;
 };
+
+// linked lists are something to wrap a head about
